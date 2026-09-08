123f4ddf0cedd99d8616151a012f53b4b9bf160e
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-09-08 17:26:57 +0300

    src/components/layout/ProtectedRoute.jsx dosyasinda admin ve personel yetkileri duzenlemesi yapildi

--- src/components/layout/ProtectedRoute.jsx
+++ src/components/layout/ProtectedRoute.jsx
@@ -1,10 +1,17 @@
+// Personel Admin Yetkilerini Goremez
+// Admin yada Personel Harici Yetkileri Goremez
+
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 
-export function ProtectedRoute({ children }) {
-  const { isAuthenticated } = useAuth();
+export function ProtectedRoute({ children, allowedRoles }) {
+  const { isAuthenticated, role } = useAuth();
   if (!isAuthenticated) {
     return <Navigate to="/giris" replace />;
+  }
+  if (allowedRoles && !allowedRoles.includes(role)) {
+    // Yetkisiz rol denemesi de bir "hata yönetimi" senaryosu
+    return <Navigate to="/" replace />;
   }
   return children;
 }
